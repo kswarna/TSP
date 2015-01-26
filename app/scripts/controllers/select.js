5ea1fd114bed39ccf9e808ba80810cb685909f77
kswarna <kswarna1985@gmail.com>
2015-01-26 06:33:26 -0500

    Drag and drop is working

--- app/scripts/controllers/select.js
+++ app/scripts/controllers/select.js
@@ -1,29 +1,26 @@
-angular.module('travelApp')
+ var App = angular.module('travelApp')
 .controller('oneCtrl', function($scope, $timeout) {
-  $scope.list1 = [];
-  $scope.list2 = [];
-  $scope.list3 = [];
-  $scope.list4 = [];
+  $scope.AvailableCities = [
+    { 'Name': 'Item 1', 'drag': true },
+    { 'Name': 'Item 2', 'drag': true },
+	{ 'Name': 'Item 3', 'drag': true },
+    { 'Name': 'Item 4', 'drag': true },
+    { 'Name': 'Item 5', 'drag': true },
+    { 'Name': 'Item 6', 'drag': true },
+    { 'Name': 'Item 7', 'drag': true },
+    { 'Name': 'Item 8', 'drag': true }
+];
+  $scope.selectedCities = [];
   
-  $scope.list5 = [
-    { 'title': 'Item 1', 'drag': true },
-    { 'title': 'Item 2', 'drag': true },
-    { 'title': 'Item 3', 'drag': true },
-    { 'title': 'Item 4', 'drag': true },
-    { 'title': 'Item 5', 'drag': true },
-    { 'title': 'Item 6', 'drag': true },
-    { 'title': 'Item 7', 'drag': true },
-    { 'title': 'Item 8', 'drag': true }
-  ];
 
-  // Limit items to be dropped in list1
-  $scope.optionsList1 = {
-    accept: function(dragEl) {
-      if ($scope.list1.length >= 2) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  };
-});
+  // // Limit items to be dropped in unselectedCities
+  // $scope.optionsunselectedCities = {
+    // accept: function(dragEl) {
+      // if ($scope.unselectedCities.length >= 2) {
+        // return false;
+      // } else {
+        // return true;
+      // }
+    // }
+  // };
+ });
