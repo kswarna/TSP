cee7e783d65dcff4170432207b3fe31e9dac2d8c
Karthik Swarna <kswarna1985@gmail.com>
2015-02-03 15:12:34 -0500

    Test

--- app/scripts/controllers/select.js
+++ app/scripts/controllers/select.js
@@ -8,6 +8,7 @@ function($scope,CityFactory,$timeout) {
 // Function to add City to Availability List 
 $scope.AddAvailableCity = function(Location)
 {
+    //Testing Webstorms ability to commit
 	CityFactory.AddAvailableCity(Location).then(
 	function(response)
 	{
@@ -19,7 +20,7 @@ $scope.AddAvailableCity = function(Location)
 //Function to Show Popup
 $scope.div_show =function()  {
 document.getElementById('abc').style.display = "block";
-}
+};
 
 //Function to Hide Popup
 $scope.div_hide = function(){
